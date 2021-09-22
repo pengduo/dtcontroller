c72fde40e87c2a233a883e5e67676871f35a46e4
linjinbao666 <linjinbao666@gmail.com>
2021-09-22 15:44:43 +0800

    update

--- README.md
+++ README.md
@@ -6,11 +6,12 @@ dtcontroller是一系列控制器的整合
 
 1. Machine
 
-
 2. Iot
 
 3. Proxy
 
 4. DtNode
 
+## 开发文档
 
+新增一种资源：`kubebuilder create api --group apps --version v1 --kind DtNode`
--- main.go
+++ main.go
@@ -100,6 +100,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Proxy")
 		os.Exit(1)
 	}
+	if err = (&controllers.DtNodeReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "DtNode")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
