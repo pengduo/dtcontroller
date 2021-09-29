9596afac113a43a6661715051642a11ebac955fa
linjinbao666 <linjinbao666@gmail.com>
2021-09-29 11:03:24 +0800

    update

--- api/v1/dtnode_types.go
+++ api/v1/dtnode_types.go
@@ -27,14 +27,16 @@ import (
 type DtNodeSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
-
-	Ip       string            `json:"ip,omitempty"`
-	Mac      string            `json:"mac,omitempty"`
-	User     string            `json:"user,omitempty"`
-	Password string            `json:"password,omitempty"`
-	TTL      string            `json:"ttl,omitempty"`
-	HostName string            `json:"hostname,omitempty"`
-	Labels   map[string]string `json:"labels,omitempty"`
+	ProviderID string            `json:"providerid,omitempty"`
+	Uid        string            `json:"uid,omitempty"`
+	Type       string            `json:"type,omitempty"`
+	Ip         string            `json:"ip,omitempty"`
+	Mac        string            `json:"mac,omitempty"`
+	User       string            `json:"user,omitempty"`
+	Password   string            `json:"password,omitempty"`
+	TTL        string            `json:"ttl,omitempty"`
+	HostName   string            `json:"hostname,omitempty"`
+	Labels     map[string]string `json:"labels,omitempty"`
 }
 
 // DtNodeStatus defines the observed state of DtNode
@@ -43,6 +45,7 @@ type DtNodeStatus struct {
 	// Important: Run "make" to regenerate code after modifying this file
 }
 
+//+kubebuilder:printcolumn:name="Uid",type=string,JSONPath=`.uid.phase`
 //+kubebuilder:printcolumn:name="Mac",type=string,JSONPath=`.mac.phase`
 //+kubebuilder:printcolumn:name="HostName",type=string,JSONPath=`.spec.hostname`
 //+kubebuilder:printcolumn:name="Ip",type=string,JSONPath=`.spec.ip`
